a8e8c38ee138a3272c44c32d70ee16788c6acda8
Arjun Patel <arjunap@gmail.com>
2012-11-10 15:03:03 -0800

    Setup SendGrid for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ BuyBox::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -65,5 +65,14 @@ BuyBox::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'buybox.heroku.com' }
+  config.action_mailer.default_url_options = {:host => 'buybox.heroku.com'}
+  ActionMailer::Base.smtp_settings = {
+      :address => "smtp.sendgrid.net",
+      :port => "25",
+      :authentication => :plain,
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => ENV['SENDGRID_DOMAIN']
+  }
+
 end
