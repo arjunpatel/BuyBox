1bef1bfb34f1797b6bbf0c64434cad1f068b2405
Arjun Patel <arjunap@gmail.com>
2012-11-17 10:10:06 -0800

    added new layout for profile pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,30 @@
 class WelcomeController < ApplicationController
+  layout :resolve_layout
 
   def index
+    #this is for applicaiton layout
+  end
 
+  def background_header
+
+  end
+
+  def background_no_header
+
+  end
+
+
+  private
+
+  def resolve_layout
+    case action_name
+    when "background_header"
+      "background_header"
+    when "background_no_header"
+      "background_no_header"
+    else
+      "application"
+    end
   end
 
 end
