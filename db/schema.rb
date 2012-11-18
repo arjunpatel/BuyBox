b3e51ec356c9d4e4c3118c9de65cb07818c8cd83
Arjun Patel <arjunap@gmail.com>
2012-11-18 14:05:07 -0800

    updated user.rb to fix errors

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -127,11 +126,6 @@ ActiveRecord::Schema.define(:version => 20121118012812) do
     t.string   "twitter_link"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "image"
-    t.string   "fb_token"
-    t.string   "location"
-    t.string   "fb_url"
-    t.string   "username"
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
@@ -142,10 +136,11 @@ ActiveRecord::Schema.define(:version => 20121118012812) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
+    t.string   "image"
+    t.string   "fb_token"
+    t.string   "location"
+    t.string   "fb_url"
+    t.string   "username"
     t.string   "provider"
     t.string   "uid"
   end
