dff850030edcf50919a1c761a69bccb285101689
Arjun Patel <arjunap@gmail.com>
2012-11-24 18:18:48 -0800

    update schema and asset pipeline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121123230926) do
+ActiveRecord::Schema.define(:version => 20121122092600) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -105,12 +105,12 @@ ActiveRecord::Schema.define(:version => 20121123230926) do
   end
 
   create_table "reviews", :force => true do |t|
-    t.integer  "reviewer_id", :limit => 255
-    t.integer  "rating",      :limit => 255
+    t.integer  "reviewer_id"
+    t.integer  "rating"
     t.text     "comment"
     t.integer  "user_id"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   add_index "reviews", ["user_id"], :name => "index_reviews_on_user_id"
