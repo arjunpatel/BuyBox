00b2d19ab9faeab242dd6b47624962c86984e5a9
Arjun Patel <arjunap@gmail.com>
2012-11-24 17:44:53 -0800

    update migrations

--- db/migrate/20121122092600_create_reviews.rb
+++ db/migrate/20121122092600_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.string :reviewer
-      t.string :rating
+      t.integer :reviewer_id
+      t.integer :rating
       t.text :comment
       t.references :user
       t.timestamps
--- db/migrate/20121123230926_change_data_type_of_reviews.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataTypeOfReviews < ActiveRecord::Migration
-  def change
-  	rename_column :reviews, :reviewer, :reviewer_id
-  	change_table :reviews do |t|  
-  	  t.change :reviewer_id, :integer
-  	  t.change :rating, :integer
-	end
-  end
-end
