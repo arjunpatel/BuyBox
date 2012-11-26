067594653258010b9364d9ca0d5275c2d203f3d7
Jake Pham <jakepham90@gmail.com>
2012-11-25 18:48:00 -0800

    Update the public profile to the correct layout.
    Update the public profile to the correct layout.

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ BuyBox::Application.routes.draw do
  
   match "/users/show.html" => "users#show", :as => :user_root
   match "/users/buyer_information" => "users#buyer_information", :as => :user_root
-  match "/users/show.html" => "users#show", :as => :user_root
-  match "/users/show.html" => "users#show", :as => :user_root
+  match "/users/connection" => "users#connection", :as => :user_root
+
   match "/users/profile/:id" => "users#profile", :as => :public_profile
   match "/product/" => "products#index", :as => :products
 
