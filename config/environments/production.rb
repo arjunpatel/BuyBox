cfd3e250b640db3c9a2ed8409afb83322036111d
Arjun Patel <arjunap@gmail.com>
2012-11-25 22:05:50 -0800

    fix heroku app crashing problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,11 +7,8 @@ BuyBox::Application.configure do
   # Full error reports are disabled and caching is turned on
 
   #Uncomment to disable full errors
-#  config.consider_all_requests_local = false
-#  config.action_controller.perform_caching = true
-
-  config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
