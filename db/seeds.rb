665e6ae3b1e0a0e08b6f9af6f529ff368eb0321d
Dong-Ju Nam <dongjunam23@gmail.com>
2012-11-29 22:26:38 -0800

    added stuff lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+Category.create(:category => "Books")
+Category.create(:category => "Online")
+Category.create(:category => "Electronics")
+Category.create(:category => "Sports & Fitness")
+Category.create(:category => "Home & Garden")
+Category.create(:category => "Clothings")
+Category.create(:category => "Movies & Music")
+Category.create(:category => "Homes")
+Category.create(:category => "Others")
