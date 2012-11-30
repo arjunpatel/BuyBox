5df3a0cbeb91f366c5db4c240f3b1a0805f5e84e
Arjun Patel <arjunap@gmail.com>
2012-11-29 17:21:59 -0800

    fix controller error realted active

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,5 @@ class Product < ActiveRecord::Base
   has_many :orders
   has_many :wishlists
   has_many :product_pictures
-  attr_accessible :product_name, :description, :brand, :price, :quantity, :zip, :category_id, :user_id, :digital_link, :popularity_index, :quantity_sold
+  attr_accessible :product_name, :description, :brand, :price, :quantity, :zip, :category_id, :user_id, :digital_link, :popularity_index, :quantity_sold, :active
 end
