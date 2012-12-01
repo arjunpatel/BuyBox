9c11cd5f3700fe918d5b54e9b14b0be81e217f7e
Jake Pham <jakepham90@gmail.com>
2012-11-30 20:57:53 -0800

    Update show page. Check if rating is valid.
    Update show page. Check if rating is valid.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,6 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @products = Product.limit(4)
-
     @seller= User.find(@product.user_id)
     render :layout => false
   end
