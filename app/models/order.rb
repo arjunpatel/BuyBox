2d9f57268b912b3c3c4c86441a3f50e825d32cd4
Jake Pham <jakepham90@gmail.com>
2012-12-02 14:47:20 -0800

    Get the overview chart to work but need to narrow down to specific seller

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
   # attr_accessible :title, :body
+  def self.total_on(date)
+  	Order.where("date(date_of_purchase) = ?", date).sum(:price)
+  end
 end
