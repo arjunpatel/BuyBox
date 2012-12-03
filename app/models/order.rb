cef0ba5b29e02fb1b315d8dd8846904bc16e2088
Jake Pham <jakepham90@gmail.com>
2012-12-02 20:47:45 -0800

    Fix the picture or product sale and purchased page
    Fix the picture or product sale and purchased page. Fix the graph

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,19 @@ class Order < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
   # attr_accessible :title, :body
-  def self.total_on(date)
-  	Order.where("date(date_of_purchase) = ?", date).sum(:price)
+  def self.total_on(date, user_id)
+  	products = User.find(user_id).products
+  	orders = []
+  	total = 0
+  	products.each do |product|
+  		orders = orders + product.orders
+  	end
+  	orders.each do |order|
+  		if order.date_of_purchase == date
+  			total = order.price
+  		end
+  	end
+
+  	total
   end
 end
