c214f27cadc0b7e7f9f705f966c6fcf8cf455f6f
Arjun Patel <arjunap@gmail.com>
2012-12-02 21:54:30 -0800

    update mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ BuyBox::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = {:host => 'buybox.heroku.com'}
+  config.action_mailer.default_url_options = {:host => 'buybox.herokuapp.com'}
   ActionMailer::Base.smtp_settings = {
       :address => "smtp.sendgrid.net",
       :port => "25",
