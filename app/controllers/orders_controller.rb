148a2651d820de84d6938f224cbc1b2a65516568
Dong-Ju Nam <dongjunam23@gmail.com>
2012-12-03 23:15:23 -0800

    confirmation page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,13 +12,12 @@ class OrdersController < ApplicationController
 	order.order_status = "Not Shipped"
 	product = Product.find(params[:product_id])
 	if order.save && product.quantity >= Integer(params[:quantity])
-	  flash[:notice] = "Your order was successful!"
 	  product.quantity = product.quantity - Integer(params[:quantity])
 	  if product.quantity == 0
 		product.active = false
 	  end
 	  product.save
-	  redirect_to(:controller => "products", :action => "index")
+	  redirect_to(:controller => "orders", :action => "confirmation", :id => order.id)
 	else
 	  render :file => File.join(Rails.root, 'public', '500.html')
 	end
@@ -44,6 +43,16 @@ class OrdersController < ApplicationController
 	@order.order_status = "Shipped"
 	@order.save
     redirect_to(:action => "sold")
+  end
+  
+  def confirmation
+	@order = Order.find(params[:id])
+	if @order.user != current_user
+		render :file => File.join(Rails.root, 'public', '500.html')
+	end
+	@product = @order.product
+	@buyer = current_user
+	@seller = @product.user
   end
   
 end
